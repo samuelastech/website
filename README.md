bb922dabdc99e3abaed9e87beae3998a61fa089f
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-01-16 13:15:14 -0300

    chore: restart with React DOM

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# My portfolio
-
-It is my simple portfolio. Please, feel free to help me improve its design and add new features
-
-*OBS: Mobile-First design, so setup your browser view*
+# My personal website
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus obcaecati nesciunt numquam eius earum, repudiandae distinctio atque corrupti magni deserunt nobis dicta, molestias cumque nulla. Illo totam neque architecto?</p>
+    </div>
+  );
+}
+
+export default App;

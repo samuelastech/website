39cd76112927178c6501e28771c8d37f34aa7928
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-06-05 14:52:36 -0300

    feat: full page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,125 @@
-import { useRef, useState } from 'react';
-// import { BsTranslate } from 'react-icons/bs';
-
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import { FaPlay } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 
 function App() {
-  const [themeIsLight, setThemeIsLight] = useState<boolean>(false);
-  // const [languageIsEnglish, setLanguageIsEnglish] = useState<boolean>(false);
-  const copyEmailRef = useRef<HTMLButtonElement>(null);
+  const [headerColor, setHeaderColor] = useState<string>('');
+  const [videoIsOpen, setVideoIsOpen] = useState<boolean>(false);
+  const mainRef = useRef<HTMLElement>(null);
 
-  const copyEmail = () => {
-    if (copyEmailRef.current) {
-      navigator.clipboard.writeText('samuel.araujo.souza@outlook.com');
-      copyEmailRef.current.textContent = 'E-mail copiado';
-      setTimeout(() => {
-        if (copyEmailRef.current) {
-          copyEmailRef.current.textContent = 'Clique para copiar o e-mail';
-        }
-      }, 3000);
-    } 
-  };
+  const toggleVideo = () => setVideoIsOpen(!videoIsOpen);
 
-  const toggleTheme = () => {
-    if (!themeIsLight) {
-      document.body.classList.add('-light');
+  const handleScroll = () => {
+    if (mainRef.current && mainRef.current.scrollTop > 0) {
+      // Light
+      setHeaderColor('text-black bg-white opacity-80');
     } else {
-      document.body.classList.remove('-light')
+      // Dark
+      setHeaderColor('bg-[rgba(0,0,0,0.9)] border-zinc-500');
     }
-    setThemeIsLight(!themeIsLight)
   };
+
+  useEffect(() => {
+    mainRef.current?.addEventListener('scroll', handleScroll);
+
+    return () => {
+      mainRef.current?.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className='main'>
-      <header className='header-content'>
-        {/* <label className={`toggle-language ${languageIsEnglish ? '-english' : ''}`}>
-          <BsTranslate className='icon' size={32} />
-        </label> */}
-        <label className='toggle-switch'>
-          <input className='input' type='checkbox' onChange={toggleTheme} data-theme-toggle />
-          <span className='slider'></span>
-        </label>
+    <main ref={mainRef} className="snap-y snap-mandatory overflow-y-scroll h-screen no-scrollbar scroll-smooth">
+      <header className={`fixed top-0 left-0 p-6 h-16 w-full flex items-center border-b font-mono transition-all duration-200 z-50 ${headerColor}`}>
+        <h1 className="font-medium text-lg transition-[color] duration-200"><span className="font-extralight">@</span>samuelastech<span className="font-extralight">()</span></h1>
       </header>
 
-      <h1 className='landing-headline'>
-        <span className='smalltext'>Olá, me chamo Samuel.</span>
-        <span className='headline'>Um desenvolvedor JavaScript que busca transformar ideias em código e mudar o mundo</span>
-      </h1>
+      <section className="relative snap-start w-full min-h-screen flex flex-col items-center justify-center p-2 text-center">
+        <h1 className="font-bold text-8xl sm:text-9xl w-full">Samuel<br />Araujo<br />Souza</h1>
+        <h2 className="text-xl sm:text-2xl font-mono tracking-widest lowercase text-purple-300">Desenvolvedor back-end</h2>
+      </section>
 
-      <div className='body-content'>
-        <p className='paragraph'>
-          Conheci o mundo do desenvolvimento de software em 2017. Aprendi lógica de programação com as táticas de guerra de Alexandre, o Grande. Pouco tempo se passou e aprendi a criar minhas primeiras interfaces com CSS, UX e UI. Em outras palavras, aprendi a dissolver problemas em soluções concretas com design.
-        </p>
+      <section className="snap-start w-full h-screen flex flex-col items-center justify-center p-6 bg-gradient-to-b from-lime-800 to-emerald-800">
+        <div className="max-w-[768px]">
+          <h1 className="font-tlou text-6xl sm:text-9xl uppercase text-center">The Last<br />of Us</h1>
+          <p className="text-zinc-300 text-lg">
+            Em 2013, foi lançado um dos melhores jogos de todos os tempos. Com ele nasceu minha vontade de se tornar um
+            game designer e fazer jogos com gráficos tão lindos como os que eu acabara de testemunhar. Comecei no
+            design, eventualmente conheci a trinca do desenvolvimento web: HTML, CSS e JavaScript. Posso dizer que esse
+            foi meu inicio no mundo do desenvolvimento
+          </p>
+        </div>
+      </section>
 
-        <p className='paragraph'>
-          Em 2018, conheci diversas linguagens de programação e os bancos de dados. Aprendi que existia algo por trás das interfaces; algo que persistia a beleza que existia nelas. Descobri que existia uma arquitetura operando por trás de tudo, tal como um organismo vivo. Criei autonomia para melhorar o mundo.
-        </p>
+      <section className="relative snap-start w-full h-screen flex flex-col items-center justify-center p-6 bg-gradient-to-b from-emerald-800 to-cyan-800">
+        <div className="max-w-[768px]">
+          <h1 className="text-4xl sm:text-6xl font-bold text-center mb-4">
+            <span className="text-base block font-mono tracking-widest uppercase">ETEC de Embu das Artes</span>
+            Desenvolvimento de Sistemas
+          </h1>
+          <p className="text-zinc-300 text-lg">
+            Em 2017, iniciei os estudos técnicos. O professor apresentou a disciplina de lógica de programação de um
+            jeito não convecional: utilizando a lógica das estratégias de batalha de Alexandre, o grande, nas guerras
+            greco-pérsicas. A narrativa não me deixava piscar. Um jeito criativo e altamente engajante de ensinar que me
+            fez ter certeza que era ali que eu queria estar.
+          </p>
+        </div>
 
-        <p className='paragraph'>
-          Você pode verificar alguns dos meus projetos no <a className='link' href='https://github.com/samuelastech' target='_blank' rel="noreferrer">GitHub</a>, se conectar comigo no <a className='link' href='https://www.linkedin.com/in/samuelastech' target='_blank' rel="noreferrer">LinkedIn</a> ou ainda me enviar um <u className='link'>e-mail</u>:
-        </p>
+        <button
+          onClick={toggleVideo}
+          className="
+            absolute
+            bottom-6
+            left-6
+            flex
+            justify-evenly
+            items-center
+            gap-2
+            border-2
+            p-4
+            w-full
+            max-w-80 
+            z-10
+            text-lg
+            overflow-hidden
+            transition-colors
+            font-medium
 
-        <button className='button-cta' ref={copyEmailRef} onClick={copyEmail}>Clique para copiar o e-mail</button>
+            hover:text-cyan-800
+            before:hover:scale-x-100
 
-        <p className='paragraph'>
-          Dentre os conhecimentos técnicos, posso listar alguns: JavaScript — React DOM, React Native, NodeJS, NestJS, MongoDB —, Docker, Kubernetes, CI/CD, Arquitetura de Sistemas...
-        </p>
+            before:content-['']
+            before:absolute
+            before:top-0
+            before:left-0
+            before:origin-top-left
+            before:scale-x-0
+            before:-z-10
+            before:h-full
+            before:w-full
+            before:transition-transform
+            before:duration-500
+            before:bg-white"
+        >
+          <FaPlay />
+          Veja a batalha de maratona
+        </button>
 
-        <p className='paragraph'>
-          O carro chefe que guia o meu trabalho em equipe é o Scrum — inspeção e adaptação constante. Mas, o denominador comum de todas as habilidades é a serenidade, foco e autoconsciência.
-        </p>
-      </div>
-    </div>
+        <div className={`absolute top-0 left-0 z-50 w-full h-full transition-all ${videoIsOpen ? 'block' : 'hidden'}`}>
+          <IoClose className="z-50 absolute top-16 right-6 text-white text-4xl border-2 rounded-full cursor-pointer duration-500 hover:bg-white hover:text-black" onClick={toggleVideo} />
+          <iframe
+            className="w-full h-full z-0"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/V3b6M5CzYTE?si=n-rdpCEPLZdG376O?autoplay=1"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen>
+          </iframe>
+        </div>
+      </section>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-700': '#633BBC',
+        'purple-300': '#8257E5'
+      },
+      fontFamily: {
+        'mono': ['Roboto Mono', 'monospace'],
+        'tlou': ['AC Compacta', 'sans serif']
+      }
+    },
   },
   plugins: [],
 }
